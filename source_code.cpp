15f24973bd4fbea8c6aad4c1afffa520d4cd319b
ThePoopLord <88780282+ThePoopLord@users.noreply.github.com>
2021-12-28 23:35:30 +0200

    Create source_code.cpp

---
+++ source_code.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+    int limit = 12;
+    int limitMinus = limit-=1;
+    int x = 1;
+    int y = 1;
+    for (int x = 1; x <= limit; x++) {
+        cout << y << " * " << x << " = " << x*y << "\n";
+        if (x == limit) {
+            x = 1;
+            if (y <= limitMinus) {
+                y+=1;
+                cout << "The first factor is currently: " << y << "\n";
+            }
+            else if (y >= limit) {
+                break;
+            }
+        }
+    }
+    return 0;
+}
