f4ee8c55347ea2bfcbb8b94d4c4c7ff61df2336a
ThePoopLord <88780282+ThePoopLord@users.noreply.github.com>
2021-12-29 15:22:34 +0200

    Update source_code.cpp
    The user can now input the limit of the multiplication table

--- source_code.cpp
+++ source_code.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 
 using std::cout;
+using std::cin;
 
 int main() {
-    int limit = 12;
+    int limit;
+    cin >> limit;
     int limitMinus = limit-=1;
+    int limitPlus = limit+=1;
     int x = 1;
     int y = 1;
-    for (int x = 1; x <= limit; x++) {
+    for (x; x <= limitPlus; x++) {
         cout << y << " * " << x << " = " << x*y << "\n";
-        if (x == limit) {
+        if (x >= limit) {
             x = 1;
-            if (y <= limitMinus) {
+            if (y < limitPlus) {
                 y+=1;
                 cout << "The first factor is currently: " << y << "\n";
             }
